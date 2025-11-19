be1c0902d8f6ccb596d8d5de109ee9fab8e93c4b
your-username <georgiosgiannopoulos1993@gmail.com>
2025-11-19 04:56:56 +0200

    Fixed mistake in signals

--- project/application/signals.py
+++ project/application/signals.py
@@ -6,8 +6,8 @@ from project.application.helpers import hash_text
 @receiver(pre_save, sender=Answer)
 @receiver(pre_save, sender=Question)
 def update_question_answers_hash(sender, instance, **kwargs):
-    sender._default_manager.filter(id=instance.id).update(hash=hash_text(instance.text))
+    instance.hash = hash_text(instance.text)
     
 @receiver(pre_save, sender=Category)
 def update_categories_hash(sender, instance, **kwargs):
-    sender._default_manager.filter(id=instance.id).update(hash=hash_text(instance.name))
+    instance.hash = hash_text(instance.name)
